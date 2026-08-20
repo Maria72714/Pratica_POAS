d6bdf570dd395795461a05eba17b6ff613af1ad8
Lucas Nóbrega <lucasfernanndo07@gmail.com>
2026-08-20 08:10:31 -0300

    refactor: atualização da tabela mediador
    * Adição de tipo de atendimento
    * Adição de campo de observações

--- backend/models/users/mediador.py
+++ backend/models/users/mediador.py
@@ -1,9 +1,12 @@
 from sqlmodel import SQLModel, table, Field, Relationship
 from typing import Optional
 from backend.models.users.user import Usuario
+from enums import TipoMediador
 
 class Mediador(SQLModel, table=True):
     __tablename__ = 'mediadores'
     id: int | None = Field(default=None, primary_key=True, foreign_key="usuarios.id")
+    tipo: TipoMediador
+    apoio_descricao: str | None = Field(max_length=250)
 
     usuario: Usuario = Relationship(back_populates="mediador")
